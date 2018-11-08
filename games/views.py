65c126f1dc2b1c9a23daaf0f00367be066dbf8a3
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-08 17:13:26 +0200

    - updated models for 'games' - added basic views: GameList and GameDetail - added basic templates: base, game_list and game_detail - updated url routes for 'games' app

--- games/views.py
+++ games/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Game
 
-# Create your views here.
+
+class GameList(ListView):
+    model = Game
+    template_name = 'games/game_list.html'
+    context_object_name = 'games'
+
+
+class GameDetail(DetailView):
+    model = Game
+    template_name = 'games/game_detail.html'
+    context_object_name = 'game'
