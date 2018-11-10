543cbb9b894d7c056f84178babbc25be36c8944f
Djet78 <vacheslavuslistyj@gmail.com>
2018-11-10 18:39:08 +0200

    - Created CustomUser model for project

--- gstaff/settings.py
+++ gstaff/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'games.apps.GamesConfig',
+    'news.apps.NewsConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+AUTH_USER_MODEL = 'users.CustomUser'
