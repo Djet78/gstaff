c4fb40a736c956903299b9a711b0d91212762655
Djet78 <vacheslavuslistyj@gmail.com>
2019-02-01 10:33:29 +0200

    - deleted SimpleOrderingAndSTR and removed dependency on this class

--- games/models.py
+++ games/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from gstaff.validators import day_is_not_future
 
 
-class SimpleOrderingAndSTR:
+class Studio(models.Model):
+    name = models.CharField(max_length=50)
+    foundation_date = models.DateField(validators=[day_is_not_future])
+    description = models.TextField()
 
     class Meta:
         ordering = ['name']
@@ -11,22 +14,28 @@ class SimpleOrderingAndSTR:
         return self.name
 
 
-class Studio(SimpleOrderingAndSTR, models.Model):
-    name = models.CharField(max_length=50)
-    foundation_date = models.DateField(validators=[day_is_not_future])
-    description = models.TextField()
-
-
-class Genre(SimpleOrderingAndSTR, models.Model):
+class Genre(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
 
+    class Meta:
+        ordering = ['name']
 
-class Platform(SimpleOrderingAndSTR, models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Platform(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        ordering = ['name']
 
-class Game(SimpleOrderingAndSTR, models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
     name = models.CharField(max_length=100)
     release_date = models.DateField()
     studio = models.ForeignKey(Studio, on_delete=models.PROTECT)
