c4daed95d46b58e794565835b36088a31ca7815b
Djet78 <vacheslavuslistyj@gmail.com>
2019-02-01 14:57:35 +0200

    - Returned folders names to Views.template_name property. Due to   unexplained property behavior.

--- games/views.py
+++ games/views.py
@@ -4,11 +4,11 @@ from .models import Game
 
 class GameList(ListView):
     model = Game
-    template_name = 'game_list.html'
+    template_name = 'games/game_list.html'
     context_object_name = 'games'
 
 
 class GameDetail(DetailView):
     model = Game
-    template_name = 'game_detail.html'
+    template_name = 'games/game_detail.html'
     context_object_name = 'game'
