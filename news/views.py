1b7995d8ab4bf43b697145da5565885eac926f33
Djet78 <vacheslavuslistyj@gmail.com>
2019-02-01 11:50:40 +0200

    - removed redunduncy from template_name's property

--- news/views.py
+++ news/views.py
@@ -4,11 +4,11 @@ from .models import Article
 
 class ArticleList(ListView):
     model = Article
-    template_name = 'news/article_list.html'
+    template_name = 'article_list.html'
     context_object_name = 'articles'
 
 
 class ArticleDetail(DetailView):
     model = Article
-    template_name = 'news/article_detail.html'
+    template_name = 'article_detail.html'
     context_object_name = 'article'
