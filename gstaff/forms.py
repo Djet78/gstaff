15b41e51aceb2a2e435b49a591b800b9586e5588
Djet78 <vacheslavuslistyj@gmail.com>
2019-03-19 09:50:50 +0200

    Customized search bar input:  - Updated TextInput widget for search field.  - Added templates: for search input and search submit button.  - Added magnifier icon for search submit btn.  - Created utils.sass file

--- gstaff/forms.py
+++ gstaff/forms.py
@@ -2,4 +2,14 @@ from django import forms
 
 
 class SearchBarForm(forms.Form):
-    search = forms.CharField(max_length=100, required=False)
+    search = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(
+            attrs={
+                'type': 'search',
+                'class': 'form-control m-2',
+                'placeholder': 'Search',
+            }
+        )
+    )
